80075ecdeca1fdeb41270ee9a67bc9d1d620c81d
carl <carltayloruk@protonmail.com>
2025-10-15 15:00:27 +0100

    - Fix JSOUP req hanging - Early return if max price not populated - Hook up stored max price to scraper

--- app/src/main/java/com/supermassivecode/vinylfinder/data/local/DiscogsWantedRecordWorker.kt
+++ app/src/main/java/com/supermassivecode/vinylfinder/data/local/DiscogsWantedRecordWorker.kt
@@ -13,19 +13,22 @@ class DiscogsWantedRecordWorker(
 
     /**
      * Search Discogs MarketPlace
-     * trigger send of local message or email (should this just watch repository for results?)
+     * TODO: trigger send of local message or email (should this just watch repository for results?)
      */
 
     suspend fun doWork() {
         //TODO add delay so not hammering website or do this externally from call site?
-        //TODO get max price from UI / DB
         wantedFoundRecordsRepository.getAllWantedRecords().map { wantedRecord ->
+            if (wantedRecord.maxPrice ==  null) { return }
 
             val url = "https://www.discogs.com/sell/release/${wantedRecord.discogsRemoteId}?sort=price%2Casc&limit=250&page=1"
-            val doc: Document = Jsoup.connect(url).get()
-
+            val doc: Document = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+                .timeout(10000) // 10 seconds
+                .followRedirects(true)
+                .get()
             scraper.scrapeRelease(
-                maxPriceIncludingShipping = 100F,
+                maxPriceIncludingShipping = wantedRecord.maxPrice,
                 localCurrencySymbol = currencyUtils.localSymbol(),
                 htmlDocument = doc,
                 originUrl = url
